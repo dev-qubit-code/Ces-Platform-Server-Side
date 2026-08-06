cb38c8f861fdcd8150e5ae47e942c77661377a50
abdullhgd54-hash <abdullh.gd54@gmail.com>
2026-08-07 01:45:55 +0300

    Student information

---
+++ Models/StudentInfo.cs
@@ -0,0 +1,16 @@
+namespace StudentsAssociation.Models;
+
+public class StudentInfo:AuditableEntity
+{
+    public string Name { get; set; } = string.Empty;
+    public string About { get; set; } = string.Empty;
+    public List<string> Skills { get; set; } = [];
+    public Dictionary<string,string> Sources { get; set; } = [];
+    public StudentInfo(string name, string about, List<string> skills, Dictionary<string,string> sources)
+    {
+        Name = name;
+        About = about;
+        Skills = skills;
+        Sources = sources;
+    }
+}
