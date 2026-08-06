d9b8e95d90c47f5a0715c716a8e466fac4725c5c
abdullhgd54-hash <abdullh.gd54@gmail.com>
2026-08-07 01:46:58 +0300

    adding teacher model

---
+++ Models/Teacher.cs
@@ -0,0 +1,13 @@
+namespace StudentsAssociation.Models;
+
+public class Teacher:AuditableEntity
+{
+    public string Name { get; set; } = string.Empty;
+    // navigation 
+    List<Test> Tests = [];
+    List<Note> Notes = [];
+    public Teacher(string name)
+    {
+        Name = name;
+    }
+}
