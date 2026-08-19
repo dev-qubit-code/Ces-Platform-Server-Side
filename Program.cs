03fe95d321ebfaae6b9630863d8e0cd1be990e67
abdullhgd54-hash <abdullh.gd54@gmail.com>
2026-08-19 20:40:02 +0300

    aplication services and middlewares

--- Program.cs
+++ Program.cs
@@ -1,6 +1,40 @@
+using SPMS_PROJECT;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddApplicationServices(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+if(!app.Environment.IsDevelopment())
+    app.UseExceptionHandler();
+
+// app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/openapi/v1.json", "CES-Platform Api");
+
+        options.EnableDeepLinking();
+        options.DisplayRequestDuration();
+        options.EnableFilter();
+    });
+}
+
+app.UseStatusCodePages();
+
+app.UseRouting();
+
+// app.UseCors();
+
+// app.UseAuthentication();
+
+// app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
