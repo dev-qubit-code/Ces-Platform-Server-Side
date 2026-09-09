cac2429bcc37a30904edce8046230226fbf9cc86
Abdullaa <a.mun00eer@gmail.com>
2026-09-09 21:09:22 +0300

    validation and migration

--- Data/Configuration/NoteConfigration.cs
+++ Data/Configuration/NoteConfigration.cs
@@ -15,9 +15,10 @@ namespace Ces_Platform_Server_Side.Data.Configuration
             builder.Property(n => n.Date).HasColumnType("date");
             builder.Property(n => n.TeacherId).HasColumnType("UNIQUEIDENTIFIER");
             builder.Property(n => n.CourseId).HasColumnType("UNIQUEIDENTIFIER");
-            
-            
 
+            builder.HasOne(n => n.Teacher)
+                .WithMany(d => d.Notes);
+            
         }
     }
 }
