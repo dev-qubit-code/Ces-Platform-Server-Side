f03692ab6c80b71a0c669a291d35acf2ea87c26b
abdullhgd54-hash <abdullh.gd54@gmail.com>
2026-09-18 01:04:17 +0300

    adding the migration: creating Report model and configuration

--- Models/Report.cs
+++ Models/Report.cs
@@ -5,7 +5,7 @@ public class Report:AuditableEntity
 
     public string TItle { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public Report(string tItle, string description)
+    public Report(string tItle, string description, string createdBy) : base(createdBy)
     {
         TItle = tItle;
         Description = description;
