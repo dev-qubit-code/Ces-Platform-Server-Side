75e0906fe3589289f95a51122db9caf72477d4d9
Abdullaa <a.mun00eer@gmail.com>
2026-09-22 22:29:45 +0300

    Add Request, Response, Validaors, Models, Requests

---
+++ Requests/Test/CreateTestRequest.cs
@@ -0,0 +1,12 @@
+﻿using Ces_Platform_Server_Side.Enums;
+
+namespace Ces_Platform_Server_Side.Requests
+{
+    public class CreateTestRequest
+    {
+        public Guid CourseId { set; get; }
+        public Guid TeacherId { set; get; }
+        public DateOnly TestDate { set; get; }
+        public TestKind kind { set; get; } = TestKind.None;
+    }
+}
