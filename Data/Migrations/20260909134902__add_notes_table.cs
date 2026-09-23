f21b92dd5ccc14c0d98693be1e8c59cb910eab95
Abdullaa <a.mun00eer@gmail.com>
2026-09-23 20:14:33 +0300

    add Data migration ,validation ,configration

---
+++ Data/Migrations/20260909134902__add_notes_table.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Ces_Platform_Server_Side.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class _add_notes_table : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Notes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CreatedAtUtc = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastModifiedAtUtc = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    LastModifiedBy = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Date = table.Column<DateOnly>(type: "date", nullable: false),
+                    TeacherId = table.Column<Guid>(type: "UNIQUEIDENTIFIER", nullable: false),
+                    CourseId = table.Column<Guid>(type: "UNIQUEIDENTIFIER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Notes", x => x.Id)
+                        .Annotation("SqlServer:Clustered", false);
+                    table.ForeignKey(
+                        name: "FK_Notes_Courses_CourseId",
+                        column: x => x.CourseId,
+                        principalTable: "Courses",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Notes_Teachers_TeacherId",
+                        column: x => x.TeacherId,
+                        principalTable: "Teachers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_CourseId",
+                table: "Notes",
+                column: "CourseId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_Name",
+                table: "Notes",
+                column: "Name",
+                unique: true,
+                filter: "[Name] IS NOT NULL");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Notes_TeacherId",
+                table: "Notes",
+                column: "TeacherId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Notes");
+        }
+    }
+}
---
+++ Validators/Note/CreateNoteRequestValidator.cs
@@ -0,0 +1,9 @@
+﻿using Ces_Platform_Server_Side.Requests;
+using FluentValidation;
+
+namespace Ces_Platform_Server_Side.Validators
+{
+    public class CreateNoteValidator: AbstractValidator<CreateNoteRequest>
+    {
+    }
+}
